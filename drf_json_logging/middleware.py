bdb46288a9dcc082639a58caa9047f6d2f838c72
Jason Lu <luhonghai@gmail.com>
2017-12-05 17:51:31 +0700

    Update logging request

--- drf_json_logging/middleware.py
+++ drf_json_logging/middleware.py
@@ -4,10 +4,29 @@ import uuid
 from django.conf import settings
 
 from request_id.middleware import RequestIdMiddleware
-
+from request_id import local, release_local
 
 logger = logging.getLogger(__name__)
 
 
 class RequestLoggingIdMiddleware(RequestIdMiddleware):
-    pass
+
+    def process_response(self, request, response):
+
+        if 'favicon' in request.path:
+            return response
+
+        user = getattr(request, 'user', None)
+        user_id = getattr(user, 'pk', None) or getattr(user, 'id', None)
+
+        message = 'method=%s path=%s status=%s'
+        args = (request.method, request.path, response.status_code)
+
+        if user_id:
+            message += ' user=%s'
+            args += (user_id,)
+
+        logger.info(message, *args)
+
+        release_local(local)
+        return response
